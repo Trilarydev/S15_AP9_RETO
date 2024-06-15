f124ace9316e955580c7abbc1f33d4c57a5cafa7
TrilaryDev <trilary.quispe@vallegrande.edu.pe>
2024-06-15 02:15:06 -0500

    cognitive service project

---
+++ src/app/pages/image-analyzer/image-analyzer.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-image-analyzer',
+  templateUrl: './image-analyzer.component.html',
+  styleUrls: ['./image-analyzer.component.css']
+})
+export class ImageAnalyzerComponent {
+
+}
